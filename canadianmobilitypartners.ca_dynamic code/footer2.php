563fbd8f58bd61ac24c95a80d90ebf0fcad61cbe
Rishi Kumar Nirmal <63533256+RishiNirmal@users.noreply.github.com>
2022-08-10 09:41:57 +0530

    Add files via upload

---
+++ canadianmobilitypartners.ca_dynamic code/footer2.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Resideo
+ */
+
+$general_settings = get_option('resideo_general_settings');
+$copyright        = isset($general_settings['resideo_copyright_field']) ? $general_settings['resideo_copyright_field'] : ''; ?>
+
+    <div class="pxp-footer">
+        <div class="container pt-100 " style="padding-bottom:30px"><?php get_sidebar('footer'); ?><?php if ($copyright != '') { ?>
+            <div class="pxp-footer-bottom mt-4 mt-md-5  d-flex justify-content-between">
+                <span class="pxp-footer-copyright col-lg-6"><?php echo esc_html($copyright); ?></span>
+                <div class="pxp-footer-copyright">Developed
+                                by |<a href="https://01synergy.com/" class="ml-1 text-white">01 Synergy</a></div>
+        </div>
+            </div><?php } ?></div>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
